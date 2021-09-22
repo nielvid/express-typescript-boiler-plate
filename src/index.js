0f0b2a9d26030104bf6a4dc89930a2b4d270ce84
nielvid <nielvidservices@gmail.com>
2021-09-22 14:52:46 +0100

    docs: ✏️ Express- typescript set up boiler-plate
    Initial setup of express app with typescript boiler-plate

---
+++ src/index.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// rest of the code remains same
+const app = (0, express_1.default)();
+const PORT = 8000;
+app.get('/home', (req, res) => res.send('Express + TypeScript Server is running fine, aRE U SURE, yes i\'m sure'));
+app.listen(PORT, () => {
+    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+// rest of the code remains same
+const app: Application   = express();
+const PORT = 5000;
+app.get('/', (request: Request, response: Response, next: NextFunction) => response.send('Express with TypeScript Server is not runnning  fine'));
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
