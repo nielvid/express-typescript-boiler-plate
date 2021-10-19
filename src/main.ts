329b5d9d2cfbd8c823b391232ff46d46ff7eb36c
nielvid <nielvidservices@gmail.com>
2021-10-19 17:03:41 +0100

    refactor: 💡 configuration files refactored
    configuration files and set up refactored

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 // rest of the code remains same
 const app: Application   = express();
 const PORT = 5000;
-app.get('/', (request: Request, response: Response, next: NextFunction) => response.send('Express with TypeScript Server is not runnning  fine'));
+app.get('/', (request: Request, response: Response, next: NextFunction) => response.send('Express with TypeScript Server is not runnning fine and fine'));
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
